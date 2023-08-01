4ad241ed409b24b12898f422b6c0fa2bb5d81d67
otmorales23 <oliverwills0227@gmail.com>
2023-07-31 17:19:00 -0700

    button now generates password based on prompts

--- script.js
+++ script.js
@@ -7,7 +7,31 @@ var special = confirm("Include special characters?");
 var passwordLength = prompt("Choose a password length between 8 and 128 characters.");
 var passwordLen = parseInt(passwordLength)
 
-
+if (passwordLen >= 8 && passwordLen <= 128) {
+  function generatePassword() {
+    var emptyString = "";
+    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    var numbers = '0123456789';
+    var symbols = '!"#$%&()*+,-./:;<=>?@^[]^_`{|}~';
+    
+    emptyString += upperLetter ? uppercase : '';
+    emptyString += lowerLetter ? lowercase : '';
+    emptyString += num ? numbers : '';
+    emptyString += special ? symbols : '';
+    
+  
+    var password = '';
+    for (var i = 0; i < passwordLen; i++) {
+      password += emptyString.charAt(Math.floor(Math.random() * emptyString.length));
+         
+    }
+    console.log(password)
+    return password;
+  }
+} else {
+  alert("Password must be between 8 and 128 characters.")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
